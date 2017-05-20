6c261f2c7343c5c6b029a8b4eb53eb3a6f728b08
jokcik <jokcik@yandex.ru>
2017-05-20 08:59:51 +0300

    Создание маленького REST api. Добавление json converter.

--- src/main/java/com/remind/config/WebConfig.java
+++ src/main/java/com/remind/config/WebConfig.java
@@ -1,9 +1,15 @@
 package com.remind.config;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 /**
  * Created by User on 19.05.2017.
@@ -12,5 +18,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 @ComponentScan("com.remind")
 public class WebConfig extends WebMvcConfigurerAdapter{
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        converter.setGson(gson);
 
+        converters.add(converter);
+    }
 }
--- src/main/java/com/remind/server/RemeindController.java
+++ src/main/java/com/remind/server/RemeindController.java
@@ -1,21 +1,26 @@
 package com.remind.server;
 
-import org.springframework.stereotype.Controller;
+import com.remind.entity.Speciality;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by User on 19.05.2017.
  */
-@Controller
+@RestController
 @RequestMapping("/remeind")
 public class RemeindController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String method(ModelMap modelMap) {
-        return "Hello!";
+    public Speciality method() {
+        return createMockSpeciality();
+    }
+
+    private Speciality createMockSpeciality() {
+        return new Speciality(1, "fafasd");
     }
 }
